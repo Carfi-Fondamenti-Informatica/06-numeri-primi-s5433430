aa66ca623973cff7e95e2587738a64352f47737b
s5433430 <115527969+s5433430@users.noreply.github.com>
2022-11-30 18:37:08 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
 
+bool nprimi (int a) {
+    bool risultato = 1;
+        for (int i=2; i<a; i++) {
+        if (a%i ==0) {
+
+        return risultato;
+
+        }
+        risultato = 0;
+        return risultato;
+    }
+}
